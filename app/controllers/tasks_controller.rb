dcc2315cbe16e833789262d7a8a327dee7d4606f
ryouga kida <06ryouga24@gmail.com>
2019-01-30 07:05:31 +0000

    add culmn

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+    before_action :set_task, only: [:show, :edit, :update, :destroy]
+    
     def index
         @tasks = Task.all
     end
@@ -16,7 +18,6 @@ class TasksController < ApplicationController
     end
     
     def show
-        @task = Task.find(params[:id])
     end
     
     def new
@@ -25,12 +26,9 @@ class TasksController < ApplicationController
     
     
     def edit
-        @task = Task.find(params[:id])
     end
     
     def update
-        @task = Task.find(params[:id])
-        
         if @task.update(task_params)
             flash[:success] = 'Task は正常に更新されました'
             redirect_to @task
@@ -41,7 +39,6 @@ class TasksController < ApplicationController
     end
     
     def destroy
-        @task = Task.find(params[:id])
         @task.destroy
         
         flash[:success] = 'Task は正常に削除されました'
@@ -51,8 +48,12 @@ class TasksController < ApplicationController
     
     private
     
+    def set_task
+        @task = Task.find(params[:id])
+    end
+    
     # Strong Parameter
     def task_params
-        params.require(:task).permit(:content)
+        params.require(:task).permit(:content, :status)
     end
 end
